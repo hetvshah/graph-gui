1b02756b84874dd6f72703bae5a2853112437a14
Matt Friedman <69323491+mattfriedman45@users.noreply.github.com>
2021-04-26 16:44:39 -0400

    Add files via upload
    Graph Class

---
+++ Graph.java
@@ -0,0 +1,36 @@
+import java.awt.List;
+
+public class Graph {
+	private boolean directed;
+	private boolean weighted;
+	
+	public boolean addEdge(String n1, String n2) {
+		return false;
+	}
+	
+	public boolean deleteEdge(String n1, String n2) {
+		return false;
+	}
+	
+	public boolean addNode(String n1) {
+		return false;
+	}
+	
+	public boolean deleteNode(String n1) {
+		return false;
+	}
+	
+	public int getOutDegree(String n1) {
+		return 0;
+	}
+	
+	public int numOfVertices() {
+		return 0;
+	}
+	
+	public List getOutgoingNeighbors(String n1) {
+		return null;
+	}
+
+	
+}
